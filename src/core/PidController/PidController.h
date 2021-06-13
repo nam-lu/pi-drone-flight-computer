fa1a8c8ad531740c9d34327de844306e2e707869
Nam Lu <42857192+nam-lu@users.noreply.github.com>
2021-06-13 19:27:05 +1000

    Update default args

--- src/core/PidController/PidController.h
+++ src/core/PidController/PidController.h
@@ -1,18 +1,17 @@
 #pragma once
 
 struct OutputLimits {
-    signed int low;
-    signed int high;
+    signed int low, high;
 };
 
 class PidController {
     public:
         PidController(
-            float kp = 1.0,
-            float ki = 0.0,
-            float kd = 0.0,
+            float kp = 1,
+            float ki = 0,
+            float kd = 0,
             signed int setpoint = 0,
-            OutputLimits outputLimits = {-10000, 10000}
+            OutputLimits outputLimits = {-1000000, 1000000}
         );
     private:
         float kp;
