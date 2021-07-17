363639ba68bdcd92badb9d9d127f75a9701ee489
nam-lu <42857192+nam-lu@users.noreply.github.com>
2021-07-17 16:23:51 +1000

    Add pigpio initialise placeholder

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include "iostream"
 #include "./core/PidController/PidController.h"
+// #include "pigpio.h"
 
 int main() {
     PidControllerInput input = {};
     PidController pid = PidController(input);
     std::cout << "Created PidController" << std::endl;
+
+    // if (gpioInitialise() < 0) {
+    //     std::cout << "pigpio initialisation failed" << std::endl;
+    // } else {
+    //     std::cout << "pigpio initialised okay" << std::endl;
+    //     gpioTerminate();
+    //     std::cout << "pigpio terminated" << std::endl;
+    // };
+
     return 0;
 }
