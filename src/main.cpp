67825bd096d1f9c01fa28bbb24608b3568d31a61
nam-lu <42857192+nam-lu@users.noreply.github.com>
2021-07-24 18:53:45 +1000

    IMU sensor testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "iostream"
 #include "./core/PidController/PidController.h"
 // #include "pigpio.h"
+// #include "RTIMULib.h"
+// #include "string"
+
+// #define R2D 57.2957732
 
 int main() {
     PidControllerInput input = {};
@@ -14,6 +18,27 @@ int main() {
     //     gpioTerminate();
     //     std::cout << "pigpio terminated" << std::endl;
     // };
+
+    // RTIMUSettings sensorSettings;
+    // RTIMULSM6DS33LIS3MDL imu(&sensorSettings);
+    // imu.IMUInit();
+    // imu.setSlerpPower(0.02);
+    // imu.setAccelEnable(true);
+    // imu.setGyroEnable(true);
+    // imu.setCompassEnable(true);
+    // while (true) {
+    //     usleep(imu.IMUGetPollInterval() * 1000);
+    //     while(imu.IMURead()) {
+    //         RTIMU_DATA imuData = imu.getIMUData();
+    //         RTVector3 curr_pose = imuData.fusionPose ;
+    //         printf("Roll = %g Pitch = %g Yaw = %g \n", curr_pose.x()*R2D, curr_pose.y()*R2D, curr_pose.z()*R2D );
+    //         // std::cout << std::to_string(imuData.accel.x() * 9.80665) + ' ' +  std::to_string(imuData.accel.y() * 9.80665) + ' ' +  std::to_string(imuData.accel.z() * 9.80665)  << std::endl;
+    //     }
+    // }
+
+    // imu.setAccelEnable(false);
+    // imu.setGyroEnable(false);
+    // imu.setCompassEnable(false);
 
     return 0;
 }
